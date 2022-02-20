0c67421740173376f5ecd32c69a2e5bb972dd864
mattiaromero <mattiaromero10@gmail.com>
2022-02-20 20:32:09 +0100

    fixed plots

--- MWC_Project_2.m
+++ MWC_Project_2.m
@@ -6,11 +6,11 @@ clear; clc; close all;
 
 %% Load the data 
 
-%Loading the files LowTide.txt, midtide and highTide.txt
+%Loading the files LowTide.txt, midTide.txt and highTide.txt
 lowTide = load('lowTide.txt');
 midTide = load('midTide.txt')
 highTide = load('highTide.txt');
-data = [lowTide midTide highTide]
+data = [lowTide midTide highTide];
 lowP1 = lowTide(:,1);
 lowP3 = lowTide(:,2);
 lowP4 = lowTide(:,3);
@@ -41,14 +41,13 @@ line([conf95Interval(1) conf95Interval(1)],[0 6],'Color','k','LineWidth',2);
 line([conf95Interval(2) conf95Interval(2)],[0 6],'Color','k','LineWidth',2);
 title('Spectrum and confidence interval for 1 block');
 xlabel('Frequency [Hz]','FontWeight','bold');
-ylabel('Spectrum [m^2/Hz]','FontWeight','bold');
+ylabel('E [m^2/Hz]','FontWeight','bold');
 legend('Spectrum','Confidence interval (95%)');
 grid on;
 savefig('Matlab2_i');
 
 %Compare with the spectra obtained using 3, 7, 15, 31 blocks. 
 figure;
-%legen = [['Spectrum (N =3)'] ['Spectrum (N = 7)'] ['Spectrum (N = 15)'] ['Spectrum (N = 31)']]
 for i=1:4
     subplot(4,1,i);
     [S f edf conf95Interval] = VarianceDensitySpectrum(lowP1,0.5^i*length(lowP1),2);
@@ -56,12 +55,22 @@ for i=1:4
     hold on;
     line([conf95Interval(1) conf95Interval(1)],[0 6],'Color','k','LineWidth',2);
     line([conf95Interval(2) conf95Interval(2)],[0 6],'Color','k','LineWidth',2); 
-    %legend(legen(i));
+    ylabel('E [m^2/Hz]','FontWeight','bold');
+    if i==1
+    title('Spectrum and confidence interval for N=3,7,15 and 31 blocks');
+    legend('Spectrum (N=3)','Confidence interval (95%) (N=3)');
+    end
+    if i==2
+    legend('Spectrum (N=7)','Confidence interval (95%) (N=7)');
+    end
+    if i==3
+    legend('Spectrum (N=15)','Confidence interval (95%) (N=15)');
+    end
+    if i==4
+    legend('Spectrum (N=31)','Confidence interval (95%) (N=31)');
+    end
 end
-title('Spectrum and confidence interval for N=3,7,15 and 31 blocks');
 xlabel('Frequency [Hz]','FontWeight','bold');
-ylabel('Spectrum [m^2/Hz]','FontWeight','bold');
-legend('Spectrum (N=3)','Confidence interval (95%) (N=3)','Spectrum (N=3)','Confidence interval (95%) (N=3)');
 grid on;
 savefig('Matlab2_ii');
 
@@ -70,52 +79,41 @@ savefig('Matlab2_ii');
 % We use 15 blocks as this is the best balance of resolution and
 % reliablility
 
-% Plot the spectra at the positions P1 to P6 for the low tide Egmond data. 
-
 % Create vector for sensor data
 lowP=[lowP1 lowP3 lowP4 lowP5 lowP6];
 
-% Compute and plot spectra at different locations 
+% Plot the spectra at the positions P1 to P6 for the low tide Egmond data 
 figure;
 for i=1:5
     subplot(5,1,i);
     [S f edf conf95Interval] = VarianceDensitySpectrum(lowP(:,i),0.5^3*length(lowP(:,i)),2);
     plot(f,S);
     ylim([0 2]);
-    
-   
-    %hold on;
-    %line([conf95Interval(1) conf95Interval(1)],[0 6],'Color','k','LineWidth',2);
-    %line([conf95Interval(2) conf95Interval(2)],[0 6],'Color','k','LineWidth',2); 
+    if i==1 
+        title('Spectra for P1, P3, P4, P5 and P6 for N=15 blocks');
+    end 
+    ylabel('E [m^2/Hz]','FontWeight','bold');
+    legend('Spectrum (N=15)');
 end
-title('Spectrum and for P1, P3, P4, P5 and P6 for N=15 blocks');
 xlabel('Frequency [Hz]','FontWeight','bold');
-ylabel('Spectrum [m^2/Hz]','FontWeight','bold');
-legend('Spectrum (N=15)');
 grid on;
 savefig('Matlab2_iii');
 
 % As we get closer to shore we have less waves with smaller frequencies
 % because waves get smaller with higher frequencies?
 
+%% Computation of spectral wave characteristics
 % The separation between infragravity-wave and sea-swell frequencies is
 % often set to 0.05 Hz. Based on the spectra at the most off-shore sensor,
-% do you feel that this choice is appropriate for Egmond? 
-% NO, should be around 0.06Hz (see below). 
-
+% we investigate if this choice is appropriate for Egmond.
 figure;
 [S f edf conf95Interval] = VarianceDensitySpectrum(lowP(:,1),0.5^3*length(lowP(:,1)),2);
 plot(f,S);
+title('Spectrum at P1 for N=15 blocks');
+ylabel('E [m^2/Hz]','FontWeight','bold');
+xlabel('Frequency [Hz]','FontWeight','bold');
 
-
-% Comment the cross-shore evolution of the spectra.
-% Infragravity waves are more present offshore? Wind and swell waves disappear close to shore...
-% NOTE: Spectrum is ENERGY 
-
-%Computation of spectral wave characteristics:
-%- if fmin = 0 Hz and fmax = fN adn fN = 1/(2*0.5) = 1Hz 
-
-
+% Whole spectrum (if fmin = 0 Hz and fmax = fN adn fN = 1/(2*0.5) = 1Hz)
 for j = 1:3
     for i=1:5
         [S f edf conf95Interval] = VarianceDensitySpectrum(data(:,i+(j-1)*5),0.5^3*length(lowP(:,i)),2);
@@ -125,10 +123,14 @@ for j = 1:3
 
     end
 end
-%Now we can compare those values of the H_m0 with the matrix H13_tot that
-%we obtained in the previous practical 
 
-%Infragravity:
+%Now we can compare those values of the H_m0 with the matrix H13_tot that
+%we obtained in the previous practical
+
+display(H_m0);
+display(x.H13_tot);
+
+%Infragravity waves:
 for j = 1:3
     for i=1:5
         [S f edf conf95Interval] = VarianceDensitySpectrum(data(:,i+(j-1)*5),0.5^3*length(lowP(:,i)),2);
@@ -139,7 +141,9 @@ for j = 1:3
     end
 end
 
-%Sea swell (In this loop we also calculate the peak period for the bonus question T_maxs)
+display(H_m0inf);
+
+%Sea swell waves (In this loop we also calculate the peak period for the bonus question T_maxs)
 for j = 1:3
     for i=1:5
         [S f edf conf95Interval] = VarianceDensitySpectrum(data(:,i+(j-1)*5),0.5^3*length(lowP(:,i)),2);
@@ -150,6 +154,8 @@ for j = 1:3
             
     end
 end
+
+display(H_m0ss);
 %Now we can compare the cross-shore evolution of H_m0inf and H_m0ss  
 save("StatisticsEgmond_2","H_m0","H_m0inf","H_m0ss");
 
